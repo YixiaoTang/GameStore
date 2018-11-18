9c385d4e4af356fa58a9c56c2beaaadb5414bc68
YixiaoTang <963726538@qq.com>
2018-11-17 21:16:41 -0800

    GameViewPager

---
+++ app/src/main/java/yixiao/gamestore/Common/GameViewPager.java
@@ -0,0 +1,29 @@
+package yixiao.gamestore.Common;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class GameViewPager extends ViewPager{
+    public GameViewPager(Context context) {
+        super(context);
+    }
+
+    public GameViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return super.onInterceptTouchEvent(event);
+        //return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+        //return false;
+    }
+}
+
