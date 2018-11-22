0c9a55777a21d7f86fed440231b4291495ca95bb
YixiaoTang <963726538@qq.com>
2018-11-22 13:53:20 -0800

    Set string in values/strings

--- app/src/main/java/yixiao/gamestore/App.java
+++ app/src/main/java/yixiao/gamestore/App.java
@@ -4,11 +4,6 @@ import com.parse.Parse;
 
 import android.app.Application;
 
-import java.util.ArrayList;
-
-import yixiao.gamestore.common.Game;
-import yixiao.gamestore.common.ReadGame;
-
 
 public class App extends Application {
     @Override
